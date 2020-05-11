ddd151aeeae5c0b42c11a94f0c83303f222793b8
jerrybuks <profkiti@gmail.com>
2020-05-11 23:45:11 +0100

    fixed admin custom claim function

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,10 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 exports.addAdminClaim = functions.https.onCall((email) =>
-	admin.auth().getUserByEmail(email).then((user) =>
-		admin
-			.auth()
-			.setCustomUserClaims(user.uid, { admin: true })
-			.then(() => {
-				message: `success ${email} has been made an admin`;
-			})
-			.catch((err) => err)
-	)
+	admin
+		.auth()
+		.getUserByEmail(email)
+		.then((user) => admin.auth().setCustomUserClaims(user.uid, { admin: true }))
+		.then(() => ({ message: `success ${email} has been made an admin` }))
+		.catch((err) => err)
 );
