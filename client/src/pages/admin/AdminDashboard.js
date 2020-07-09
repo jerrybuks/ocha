f4b8b252ee8d78270ba7bc326fbbdc43f700404c
jerrybuks <profkiti@gmail.com>
2020-07-10 00:15:04 +0100

    setup cloud function for generating uuid

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import SideNav from '../../components/sidebar';
+import { Button } from '@material-ui/core';
+import QRCode from "qrcode.react";
+import { functions } from '../../firebase/firebase.utils';
+
 
 export default function AdminDashboard() {
+    const handleClick = () =>{
+       const func = functions.httpsCallable('createUUID')
+       func().then((res) => console.log(res));
+        
+    }
+    
+    const arr = ["9f0c2ce1-ce97-4c8a-9807-28017daeb478","c340812c-b2c1-4b66-8891-20db9d909a56"]
     return (
         <div>
             <div>
@@ -10,7 +21,11 @@ export default function AdminDashboard() {
                     ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
                     facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
                     gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+                    <Button onClick={handleClick}>test uud</Button>
+                    {arr.map(val => (<div><QRCode value={val} /></div>))}
                 </SideNav>
+               
+                
             </div>
         </div>
     )
